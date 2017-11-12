7944f4d253b87f624f053617414d1bb437b36a98
milos ostojic <ostojicmilos93@gmail.com>
2017-11-12 16:43:11 +0100

    First commit

---
+++ src/main/java/com/ftn/isaproject/IsaprojectApplication.java
@@ -0,0 +1,12 @@
+package com.ftn.isaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsaprojectApplication.class, args);
+	}
+}
