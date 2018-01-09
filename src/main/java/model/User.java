0d0cfa4f2cf23cd344e5f6b3b4c52faf563b893f
Dić <dusan.rodic@yahoo.com>
2018-01-09 14:41:06 +0100

    a

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,14 +1,19 @@
 package model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import enums.Role;
 
 @Entity
-public class User {
+public class User implements Serializable{
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String surname;
