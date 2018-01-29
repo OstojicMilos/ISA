3544ae2e0742161f618517a7a9103eda9f5694ba
milos ostojic <ostojicmilos93@gmail.com>
2018-01-29 20:56:42 +0100

    Added getRole method to userRepository

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -2,7 +2,9 @@ package repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import enums.Role;
 import model.User;
@@ -15,5 +17,7 @@ public interface UserRepository extends CrudRepository<User, Integer>{
 	public User findByNameOrSurname(String name, String surname);
 	public List<User> findByNameLike(String name);
 	public List<User> findBySurnameLike(String surname);
-	
+	@Query("SELECT u.role FROM User u WHERE u.id=:userId")
+	public Role getRole(@Param("userId") int userId);
+
 }
