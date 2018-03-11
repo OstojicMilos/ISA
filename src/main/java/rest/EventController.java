a5ec384f9289b4055f74095147b1985579fad50f
bogdan-todorovic <bogdantodorovic93@gmail.com>
2018-03-11 21:20:09 +0100

    endpoint za dodavanje nove projekcije u okviru eventa

--- src/main/java/rest/EventController.java
+++ src/main/java/rest/EventController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import dto.EventDetailsDto;
 import model.Event;
+import model.EventDetails;
 import service.EventService;
 
 @RestController
@@ -23,11 +25,22 @@ public class EventController {
 	@RequestMapping(method = RequestMethod.POST, value = "/establishments/{establishmentId}/events")
 	public ResponseEntity<?> createEvent(@PathVariable Integer establishmentId, @RequestBody Event event) {
 		Event createdEvent = eventService.createEvent(establishmentId, event);
-		if (createdEvent == null) {
+		if (createdEvent == null)
 			return ResponseEntity.notFound().build();
-		}
+		
 		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(createdEvent.getId()).toUri();
 		return ResponseEntity.created(location).body(createdEvent);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/events/{eventId}/details")
+	public ResponseEntity<?> addEventDetails(@PathVariable Integer eventId, @RequestBody EventDetailsDto eventDetails) {
+		EventDetails addedDetails = eventService.addEventDetails(eventId, eventDetails);
+		if (addedDetails == null)
+			return null;
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(addedDetails.getId()).toUri();
+		return ResponseEntity.created(location).body(addedDetails);
 	}
 }
