7bc8d0daa6b0da376ef5c1ae4c0f624172a1d8fe
Dić <dusan.rodic@yahoo.com>
2018-04-17 18:32:03 +0200

    Dodato biranje datuma za bioskope

--- src/main/java/service/EstablishmentService.java
+++ src/main/java/service/EstablishmentService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import enums.EstablishmentType;
 import model.Establishment;
 import model.Event;
+import model.EventDetails;
 import repository.EstablishmentRepository;
 
 @Service
@@ -17,12 +20,45 @@ public class EstablishmentService {
 	EstablishmentRepository establishmentRepository;
 	
 	public List<Establishment> getAllEstablishmentsByType(EstablishmentType type) {
-		return establishmentRepository.findByType(type);
+		List<Establishment> temp =establishmentRepository.findByType(type);
+		return temp;
 	}
 
-	public List<Event> getEvents(Integer establishmentId) {
+	public List<Event> getCinemaEvents(Integer establishmentId, Integer dayOffset) {
 		Establishment e = establishmentRepository.findOne(establishmentId);
-		if (e == null) return null;
+		if (e == null || e.getType() != EstablishmentType.CINEMA) return null;
+		ArrayList<Event> result = new ArrayList<>();
+		for(Event ev : e.getEvents()) {
+			
+			ArrayList<EventDetails> eventsOnGivenDay = new ArrayList<>();
+			
+			for(EventDetails ed : ev.getSchedule()) {
+				
+				Calendar day = Calendar.getInstance();
+				day.add(Calendar.DATE, dayOffset);
+				
+				Calendar eventDay = Calendar.getInstance();
+				eventDay.setTime(ed.getDateAndTime());
+				
+				boolean sameDay = (day.get(Calendar.YEAR) == eventDay.get(Calendar.YEAR)) &&
+									(day.get(Calendar.DAY_OF_YEAR) == eventDay.get(Calendar.DAY_OF_YEAR));
+	
+				if (sameDay) {
+					eventsOnGivenDay.add(ed);
+				}
+			}
+			if(!eventsOnGivenDay.isEmpty()) {
+				ev.setSchedule(eventsOnGivenDay);
+				result.add(ev);
+			}
+		}
+		return result;
+	}
+	
+	public List<Event> getTheatreEvents(Integer establishmentId) {
+		Establishment e = establishmentRepository.findOne(establishmentId);
+		if (e == null  || e.getType() != EstablishmentType.THEATRE)  return null;
+		
 		return e.getEvents();
 	}
 }
--- src/main/webapp/javascript/controllers/establishment-controller.js
+++ src/main/webapp/javascript/controllers/establishment-controller.js
@@ -22,9 +22,25 @@ angular.module("isaProject")
     })();
 }])
 
-.controller("RepertoireController", ["repertoirePromise", function(repertoirePromise) {
+.controller("RepertoireController", ["repertoirePromise","$route", function(repertoirePromise, $route) {
     
     var self = this;
+    var today = new Date();
+    var today_plus1 = new Date(); today_plus1.setDate(today.getDate() + 1);
+    var today_plus2 = new Date(); today_plus2.setDate(today.getDate() + 2);
+    var today_plus3 = new Date(); today_plus3.setDate(today.getDate() + 3);
+    var today_plus4 = new Date(); today_plus4.setDate(today.getDate() + 4);
+    
+    self.days = [today,
+    			today_plus1,
+    			today_plus2,
+    			today_plus3,
+    			today_plus4];
+ 
+    self.cinemaId = $route.current.params.id;
+   
+    
     self.projections = repertoirePromise;
     console.log(self.projections);
+    
 }]);
